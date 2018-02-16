51b830190ab5744a353f64ffb60cfde2315a9fc6
Rick Smit <rick@vigour.io>
2018-02-16 14:01:39 +0100

    changed url

--- server/src/index.js
+++ server/src/index.js
@@ -14,8 +14,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      // endpoint: 'https://eu1.prisma.sh/rick/hackernews-graphql-js/dev',
-      endpoint: 'http://localhost:4466/hackernews-graphql-js/dev',
+      endpoint: 'https://eu1.prisma.sh/rick/hackernews-graphql-js/dev',
       secret: 'mysecret123'
     })
   })
