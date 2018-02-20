5bf98dfff66bb30154d771b470189b9dd711c68f
Rick Smit <rick@vigour.io>
2018-02-20 14:30:59 +0100

    moved server to own repo

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -3,17 +3,9 @@ import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
 import { setContext } from 'apollo-link-context'
 import { ApolloLink } from 'apollo-link'
 
-const URL =
-  process.env.API_URL || 'https://hackernews-graphql-js-wypfehbtjp.now.sh'
-
-console.log(
-  '✨URL',
-  process.env.REACT_NATIVE_ENVIRONMENT_CURRENT,
-  process.env.API_URL,
-  URL
-)
-
-const httpLink = new HttpLink({ uri: 'http://192.168.1.241:4000' })
+const httpLink = new HttpLink({
+  uri: process.env.API_URL || 'https://coolcast-server.now.sh'
+})
 
 const authLink = setContext(async (req, { headers }) => {
   const token = await AsyncStorage.getItem('graphcoolToken')
